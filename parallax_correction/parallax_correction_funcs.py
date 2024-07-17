e667e4d9bf5292a2754bf06ba43cb39dc07280b5
chug-along <divyansh.chug@gmail.com>
2024-07-17 17:16:59 -0400

    Update parallax_correction_funcs.py
    Modified path to ERA5 data in parallax correction code

--- parallax_correction/parallax_correction_funcs.py
+++ parallax_correction/parallax_correction_funcs.py
@@ -93,7 +93,7 @@ def era_Tlapse_height(month, year, temp, lon, lat):
     :param lat: latitude
     :return: approximate height above surface
     """
-    path  = '/data/keeling/a/dchug2/a/ERA5/monthly/nwindia_z_t/'
+    path  = '../data/ERA5_yearly_NWIndia/'
     file = path + 'geopot_temperature_' + str(year) + '.nc'  # path to ERA5
     da = xr.open_dataset(file)
     da = da.sel(longitude=lon, latitude=lat, method='nearest')
